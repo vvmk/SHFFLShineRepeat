77c206bad9c7ce9bfe1aca6ba5dfb7206d1050bb
vvmk <vincentmasiello@gmail.com>
2018-05-08 22:29:56 -0400

    Create UserService scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DrillRunnerComponent } from './drill-runner/drill-runner.component';
 import { RoutineListComponent } from './routine-list/routine-list.component';
 import { RoutineListItemComponent } from './routine-list-item/routine-list-item.component';
 import { RoutineViewComponent } from './routine-view/routine-view.component';
-
+import { EndpointService } from './endpoint.service';
+import { RoutineService } from './routine.service';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,13 @@ import { RoutineViewComponent } from './routine-view/routine-view.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+      EndpointService,
+      RoutineService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
