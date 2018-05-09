d344e9d31b59eb9b402fa6edd8488d4c4f142501
vvmk <vincentmasiello@gmail.com>
2018-05-08 23:05:54 -0400

    Subscribe to user's library

--- src/app/routine-list/routine-list.component.ts
+++ src/app/routine-list/routine-list.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { RoutineService } from '../routine.service';
 import { Routine } from '../interfaces/routine';
+import { User } from '../interfaces/user';
 
 @Component({
     selector: 'app-routine-list',
     templateUrl: './routine-list.component.html',
-    styleUrls: ['./routine-list.component.css'],
-    providers: [RoutineService]
+    styleUrls: ['./routine-list.component.css']
 })
 export class RoutineListComponent implements OnInit {
     routines: Routine[];
+    userId: string;
+    errorMessage: string;
 
-    constructor(private _routineService: RoutineService) {}
+    constructor(private _routineService: RoutineService, private _user: User) {}
 
     ngOnInit() {
-        this.routines = this._routineService.getUserRoutines();
-    }
+        this._routineService.getUserRoutines(this.userId)
+        .subscribe(
+            routines => this.routines = routines,
+            error => this.errorMessage = <any>error);
+   }
 }
