480cfe5fb6912b4e13e2ee37619d6826c6c3c6b2
vvmk <vincentmasiello@gmail.com>
2018-05-08 22:43:26 -0400

    Set up UserService request

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EndpointService } from './endpoint.service';
+import { User } from './interfaces/user';
+import { Observable } from 'rxjs/observable';
 
 @Injectable()
 export class UserService {
+    //for testing until authentication added to service tier
+    userId: string = '1';
 
-  constructor() { }
+    constructor(private _http: HttpClient, private _e: EndpointService) { }
 
+    getUser(): Observable<User> {
+        return this._http.get<User>(this._e.getUserMetaUrl(this.userId));
+    }
 }
