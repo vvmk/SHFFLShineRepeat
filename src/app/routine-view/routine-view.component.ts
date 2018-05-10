c6c67e52f10c8a1fa1fec9ed4471eff396f0c4c9
vvmk <vincentmasiello@gmail.com>
2018-05-09 21:27:56 -0400

    Set up dev wireframe routine-view

--- src/app/routine-view/routine-view.component.ts
+++ src/app/routine-view/routine-view.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Routine } from '../interfaces/routine';
 
 @Component({
-  selector: 'app-routine-view',
-  templateUrl: './routine-view.component.html',
-  styleUrls: ['./routine-view.component.css']
+    selector: 'app-routine-view',
+    templateUrl: './routine-view.component.html',
+    styleUrls: ['./routine-view.component.css']
 })
 export class RoutineViewComponent implements OnInit {
+    pageTitle: string = '[Routine title]';
+    routine: Routine;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    runRoutine() {
+
+    }
 }
