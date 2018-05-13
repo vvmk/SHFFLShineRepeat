2cf70db2323f2816f22aa6f89fc1a7fcc57112ab
vvmk <vincentmasiello@gmail.com>
2018-05-13 14:05:43 -0400

    Refactor routine-view and set up routing

--- src/app/routine-view/routine-view.component.ts
+++ src/app/routine-view/routine-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Routine } from '../interfaces/routine';
+import { ActivatedRoute } from '@angular/router';
+import { RoutineService } from '../routine.service';
 
 @Component({
     selector: 'app-routine-view',
@@ -10,12 +12,15 @@ export class RoutineViewComponent implements OnInit {
     pageTitle: string = '[Routine title]';
     routine: Routine;
 
-    constructor() { }
+    constructor(private routineService: RoutineService,
+                private _route: ActivatedRoute) {}
 
     ngOnInit() {
+        let id = this._route.snapshot.paramMap.get('id');
+        this.routine = this.routineService.getRoutineById(id);
     }
 
     runRoutine() {
-
+        // navigate to drill runner and pass drill list/routine meta
     }
 }
--- src/app/routine.service.ts
+++ src/app/routine.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Routine } from './interfaces/routine';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { EndpointService } from './endpoint.service';
+import { UserService } from './user.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
@@ -9,13 +10,17 @@ import 'rxjs/add/operator/do';
 
 @Injectable()
 export class RoutineService {
+    routineLIbrary: Routine[];
 
     constructor(private _http: HttpClient, private _e: EndpointService) {
-        // -_-
+        //TODO: check local storage for user's library, set it if found
+        // else: this.routineLibrary = this.getUserRoutines();
     }
 
+    //TODO: Get the users library and store it locally
     getUserRoutines(userId: string): Observable<Routine[]> {
         let url: string = this._e.getLibraryUrl(userId);
+
         return this._http.get<Routine[]>(url)
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
@@ -24,5 +29,9 @@ export class RoutineService {
     private handleError(err: HttpErrorResponse): Observable<Routine[]> {
         console.log(err.message);
         return Observable.throw(err.message);
+    }
+
+    getRoutineById(routineId: string): Routine {
+        return routineId[routineId];
     }
 }
