6e57b59b9d6be7bd154b8a371e8a6af3208c1703
vvmk <vincentmasiello@gmail.com>
2018-05-13 14:51:14 -0400

    refmt

--- src/app/services/routine-view-guard.service.ts
+++ src/app/services/routine-view-guard.service.ts
@@ -4,18 +4,18 @@ import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 @Injectable()
 export class RoutineViewGuardService implements CanActivate {
 
-  constructor(private _router: Router) { }
+    constructor(private _router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-      let id = route.url[1].path;
-      if (!this.isValidRoutineId(id)) {
-          this._router.navigate(['/library']);
-          return false;
-      }
-      return true;
-  }
+    canActivate(route: ActivatedRouteSnapshot): boolean {
+        let id = route.url[1].path;
+        if (!this.isValidRoutineId(id)) {
+            this._router.navigate(['/library']);
+            return false;
+        }
+        return true;
+    }
 
-  isValidRoutineId(id: string) {
+    isValidRoutineId(id: string) {
       //TODO: validate routine id path variable
       return true;
   }
