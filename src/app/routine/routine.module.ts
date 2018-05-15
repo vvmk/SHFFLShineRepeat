6e03ab7ef85dc8b54b368a8093c5dafd624471c8
vvmk <vincentmasiello@gmail.com>
2018-05-15 12:02:53 -0400

    Move LibraryPageComponent to routine module

--- src/app/routine/routine.module.ts
+++ src/app/routine/routine.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { RoutineListComponent } from './routine-list/routine-list.component';
 import { RoutineListItemComponent } from './routine-list-item/routine-list-item.component';
 import { RoutineViewComponent } from './routine-view/routine-view.component';
+import { RoutineViewGuardService } from '../services/routine-view-guard.service';
+import { LibraryPageComponent } from './library-page/library-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+        { 
+            path: 'routine/:id',
+            canActivate: [RoutineViewGuardService],
+            component: RoutineViewComponent
+        },
+        {
+            path: 'library',
+            component: LibraryPageComponent
+        }
+    ])
   ],
   declarations: [
+      LibraryPageComponent,
       RoutineListComponent,
       RoutineListItemComponent,
       RoutineViewComponent
