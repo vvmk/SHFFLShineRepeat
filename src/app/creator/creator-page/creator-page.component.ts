7287c084fe9c9ffd082777a8d76e44ae36f356b4
vvmk <vincentmasiello@gmail.com>
2018-05-19 17:37:14 -0400

    Set up routing to creator page

--- src/app/creator/creator-page/creator-page.component.ts
+++ src/app/creator/creator-page/creator-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-creator-page',
   templateUrl: './creator-page.component.html',
   styleUrls: ['./creator-page.component.css']
 })
--- src/app/creator/creator.module.ts
+++ src/app/creator/creator.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { CreatorPageComponent } from './creator-page/creator-page.component';
 import { RoutineFormComponent } from './routine-form/routine-form.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+        { path: 'create', component: CreatorPageComponent }
+    ])
   ],
   declarations: [CreatorPageComponent, RoutineFormComponent]
 })
