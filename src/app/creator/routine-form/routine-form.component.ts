12d7d0488820c8eda215c89f9a8e3d7f429a823e
vvmk <vincentmasiello@gmail.com>
2018-05-22 16:18:02 -0400

    Remove buttons on drill form

--- src/app/creator/routine-form/routine-form.component.ts
+++ src/app/creator/routine-form/routine-form.component.ts
@@ -56,6 +56,10 @@ export class RoutineFormComponent implements OnInit {
         });
     }
 
+    removeDrill(id: number): void {
+        this.drills.removeAt(id);
+    }
+
     save() {
         console.log('saving: ' + this.routineForm);
     }
