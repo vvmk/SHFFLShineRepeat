4359ebcf9d9314229db14f11eac0549b16c3b99d
vvmk <vincentmasiello@gmail.com>
2018-05-28 23:37:36 -0400

    Generate login page, login/register components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [
+        declarations: [
+        LoginPageComponent,
+        RegisterFormComponent,
+        LoginFormComponent
     ]
 })
 export class UserModule { }
