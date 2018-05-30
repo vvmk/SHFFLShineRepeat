3a779f6ef6bef76aae7ca5e55863b9e362b16496
vvmk <vincentmasiello@gmail.com>
2018-05-29 21:47:06 -0400

    Generate 404 page

---
+++ src/app/four-oh-four.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ssr-four-oh-four',
+  templateUrl: './four-oh-four.component.html',
+  styles: []
+})
+export class FourOhFourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
