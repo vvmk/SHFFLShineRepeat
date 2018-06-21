d7df7a95cd437a9cc54fcb7293fcb90cb28addcc
vvmk <vincentmasiello@gmail.com>
2018-06-20 23:30:01 -0400

    drill runner things

--- src/app/runner/runner.module.ts
+++ src/app/runner/runner.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../shared/material.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { DrillRunnerComponent } from './drill-runner/drill-runner.component';
 import { RoutineResolverService } from '../services/routine-resolver.service';
@@ -9,6 +10,7 @@ import { RoutineResolverService } from '../services/routine-resolver.service';
 @NgModule({
     imports: [
         CommonModule,
+        NgbModule,
         RouterModule.forChild([
             { 
                 path: 'runner/:id',
