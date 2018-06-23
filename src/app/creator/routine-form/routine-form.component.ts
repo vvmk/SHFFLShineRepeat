b02bb1b29835d40ce9f0aa718fbc708b5cb51adc
vvmk <vincentmasiello@gmail.com>
2018-06-23 15:27:22 -0400

    change form fields to match interface
    saving the edit form properly updates the object

--- src/app/creator/routine-form/routine-form.component.ts
+++ src/app/creator/routine-form/routine-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Routine } from '../../interfaces/routine';
 import { UserService } from '../../services/user.service';
+import { RoutineService } from '../../services/routine.service';
 import { RosterService } from '../../services/roster.service';
 import { RoutineFormGuard } from '../../services/routine-guard.service';
 
@@ -19,22 +21,26 @@ export class RoutineFormComponent implements OnInit {
         return <FormArray>this.routineForm.get('drills');
     }
 
-    constructor(private _userService: UserService,
+    constructor(
+        private userService: UserService,
+        private routineService: RoutineService,
         private rosterService: RosterService,
         private guard: RoutineFormGuard,
-        private fb: FormBuilder) { }
+        private fb: FormBuilder,
+        private router: Router
+    ) { }
 
     ngOnInit() {
         this.roster = this.rosterService.getRoster();
         this.routineForm = this.fb.group({
-            routineTitle: ['',
+            title: ['',
                 [
                     Validators.required,
                     Validators.minLength(1),
                     Validators.maxLength(50)
                 ]
             ],
-            routineCharacter: ['', Validators.required],
+            character: ['', Validators.required],
             drills: this.fb.array([ this.buildDrill() ])
         });
 
@@ -47,12 +53,12 @@ export class RoutineFormComponent implements OnInit {
 
             // TODO: I suspect some fuckery populating the select box.
             this.routineForm.patchValue({
-                routineTitle: this.routine.title,
-                routineCharacter: this.routine.character
+                title: this.routine.title,
+                character: this.routine.character
             });
 
             this.routine.drills.forEach(d => {
-                this.drills.push(this.buildDrill(d.drill_title, ''+d.duration));
+                this.drills.push(this.buildDrill(d.drill_title, d.duration));
             });
         }
     }
@@ -62,14 +68,14 @@ export class RoutineFormComponent implements OnInit {
     }
 
 
-    buildDrill(title: string = '', duration: string = ''): FormGroup {
+    buildDrill(title: string = null, duration: number = null): FormGroup {
         return this.fb.group({
-            drillTitle: [title, [
+            drill_title: [title, [
                 Validators.required,
                 Validators.minLength(1),
                 Validators.maxLength(50)
             ]],
-            drillDuration: [duration, [
+            duration: [duration, [
                 Validators.required,
                 Validators.min(1),
                 Validators.max(5940)
@@ -82,6 +88,32 @@ export class RoutineFormComponent implements OnInit {
     }
 
     save(): void {
-        console.log('saving: ' + this.routineForm);
+        if (this.routineForm.dirty && this.routineForm.valid) {
+            
+            // copy the new values over the routine object values
+            let r = Object.assign({}, this.routine, this.routineForm.value);
+            r = this.setRoutineTotalDuration(r);
+
+            console.log("saving object: ", r);
+
+            this.routineService.saveRoutine(r)
+                .subscribe(() => this.onSaveComplete());
+
+        } else if (!this.routineForm.dirty) {
+            this.onSaveComplete();
+        }
+    }
+
+    setRoutineTotalDuration(r: Routine): Routine {
+        r.total_duration = r.drills.reduce((a, c) => {
+            return a + c.duration;
+        }, 0);
+
+        return r;
+    }
+
+    private onSaveComplete(): void {
+        this.routineForm.reset();
+        this.router.navigate(['/library']);
     }
 }
--- src/app/services/routine.service.ts
+++ src/app/services/routine.service.ts
@@ -6,7 +6,7 @@ import { map, catchError, tap } from 'rxjs/operators';
 
 import { Injectable } from '@angular/core';
 import { Routine } from '../interfaces/routine';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { EndpointService } from './endpoint.service';
 import { UserService } from './user.service';
 
@@ -16,11 +16,11 @@ export class RoutineService {
     routineLibrary: Routine[] = [];
 
     constructor(private http: HttpClient, 
-        private _e: EndpointService,
+        private es: EndpointService,
         private userService: UserService) {}
 
     public getUserRoutines(): ReplaySubject<Routine[]> {
-        const url = this._e.getLibraryUrl(this.userService.userId);
+        const url = this.es.getLibraryUrl(this.userService.userId);
 
         this.http.get<Routine[]>(url).subscribe(res => {
             this.routineSubscription.next(res);
@@ -38,26 +38,43 @@ export class RoutineService {
             return of(this.initializeRoutine());
         }
 
-        const url = this._e.getRoutineByIdUrl(routineId);
+        const url = this.es.getRoutineByIdUrl(routineId);
         return this.http.get<Routine>(url).pipe(
             catchError(this.handleError)
         );
     }
 
-    public createRoutine(routine: Routine): Observable<Routine> {
+    private createRoutine(routine: Routine, options): Observable<Routine> {
         return of(null);
     }
 
-    public updateRoutine(routine: Routine): Observable<Routine> {
-        return of(null);
+    private updateRoutine(routine: Routine, options): Observable<Routine> {
+        const url = this.es.getRoutineByIdUrl(routine.routine_id);
+
+        return this.http.put(url, routine, options).pipe(
+            map(() => routine),
+            catchError(this.handleError)
+        );
     }
 
+    // TODO: deleteRoutine
     public deleteRoutine(routineId: number): Observable<Response> {
         return of(null);
     }
 
     public isValidRoutineId(id: number): boolean {
         return id < this.routineLibrary.length;
+    }
+
+    public saveRoutine(routine: Routine): Observable<Routine> {
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        const options = { headers: headers };
+
+        if (routine.routine_id === 0) {
+            return this.createRoutine(routine, options);
+        }
+
+        return this.updateRoutine(routine, options);
     }
 
     private initializeRoutine(): Routine {
@@ -74,6 +91,7 @@ export class RoutineService {
         };
     }
 
+    // TODO: handleError
     private handleError(error: Response): Observable<any> {
         return null;
     }
