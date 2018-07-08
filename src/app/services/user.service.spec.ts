c37baf493a386fffe95d5e6fd55d48b1a40ef51f
vvmk <vincentmasiello@gmail.com>
2018-07-08 11:56:00 -0400

    UserService load test passes
    Still need to add unit tests

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { EndpointService } from './endpoint.service';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
+
+  const mEndpointService = jasmine.createSpyObj('EndpointService', ['userURL']);
+  const mHttpClient = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+  const mAuthService = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      providers: [UserService,
+      { provide: HttpClient, useValue: mHttpClient },
+      { provide: EndpointService, useValue: mEndpointService },
+      { provide: AuthService, useValue: mAuthService }
+      ]
     });
   });
 
