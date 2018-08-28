526c8cc0c75f7cef4858d85d90c8d4d40470c8b5
vvmk <vincentmasiello@gmail.com>
2018-08-28 10:36:57 -0400

    Home page banner registration form connected, untested

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { faStopwatch, faEdit, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { RosterService } from '../services/roster.service';
+import { UserService } from '../services/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faStopwatch, faEdit, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'ssr-home-page',
@@ -12,14 +16,64 @@ export class HomePageComponent implements OnInit {
   faStopwatch = faStopwatch;
   faEdit = faEdit;
   faSearch = faSearch;
-    roster: string[];
+  roster: string[];
+
+  registerForm: FormGroup;
+
+  loading = false;
+  submitted = false;
 
   constructor(
     private rosterService: RosterService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
   ) { }
 
   ngOnInit() {
-      this.roster = this.rosterService.getRoster();
+    this.roster = this.rosterService.getRoster();
+
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20)
+        ]
+      ],
+      tag: ['',
+        [
+          Validators.required,
+          Validators.maxLength(20)
+        ]
+      ],
+      main: ['', Validators.required]
+    });
+
   }
 
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      // TODO: navigate to register page, prefilling values to
+      // indicate bad validation status
+      this.router.navigate(['/register']);
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.loading = false;
+        });
+  }
 }
